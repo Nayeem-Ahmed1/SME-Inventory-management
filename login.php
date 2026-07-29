cf99ed7fa66f1fb3b032f7c6d3de66f644dbd395
Nayeem-Ahmed1 <nabil97773@gmail.com>
2026-07-30 03:02:08 +0600

    login_ui

--- login.php
+++ login.php
@@ -1,11 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  original login page
+<?php 
+  $title = 'Login Page';
+
+  include __DIR__ . '/views/header.php';
+?>
+
+  <div class="outer_container_login">
+      <div class="login_card">
+        <h2>Login</h2>
+        <form action="" method="post">
+          <div class="email_container">
+            <Label for="email">EMAIL :</Label>
+            <input id="email" type="text"/>
+          </div>
+          <div class="password_container">
+            <Label for="pass">PASSWORD:</Label>
+            <input id="pass" type="password"/>
+          </div>
+          <button type="submit">LOGIN</button>
+        </form>
+      </div>
+</div>
+
 </body>
 </html>
