be8aa1abacec74859e93d8b401e4c4601bcf2066
Nayeem-Ahmed1 <nabil97773@gmail.com>
2026-08-04 02:42:17 +0600

    sidebar-initial

--- dashboard.php
+++ dashboard.php
@@ -7,13 +7,47 @@
   include __DIR__ . '/views/header.php';
 ?>
 
+<?php 
+  $sidebarArr = [
+    'Dashboard' => 'fa-solid fa-gauge-high',
+    'Admins' => 'fa-solid fa-user-tie',
+    'Employees' => 'fa-solid fa-address-card',
+    'Suppliers' => 'fa-solid fa-truck-field',
+    'Products' => 'fa-solid fa-cube',
+    'Purchases' =>'fa-solid fa-cart-arrow-down',
+    'Sales' => 'fa-solid fa-money-bill-1',
+    'Warehouse' => 'fa-solid fa-warehouse',
+    'Inventory' => 'fa-regular fa-house',
+    'Customers' => 'fa-solid fa-user',
+    'Catagories' => 'fa-solid fa-table-list',
+    'Roles' => 'fa-solid fa-user-gear',
+    'Units' => 'fa-solid fa-angle-up',
+    'Brands' => 'fa-brands fa-gg-circle'];
+
+  $keys = array_keys($sidebarArr);
+?>
+
   <div class="dashboard_container">
-    <div class="nav"></div>
-    <div class="sidebar"></div>
-    <div class="main"></div>
+    <nav></nav>
+    <aside>
+      <?php foreach($keys AS $key) : ?>
+      <a href="">
+        <div class="item">
+          <div>
+            <i class="<?php echo $sidebarArr[$key] ?>"></i>
+            <?php echo $key ?>
+          </div>
+          <span>></span> 
+        </div>
+      </a>
+      <?php endforeach; ?>
+    </aside>
+    <div class="main">
+
+    </div>
   </div>
 
 </body>
 </html>
 
-<?php else : header('Location: login.php'); die(); endif ?>
+<?php else : header('Location: login.php'); die(); endif; ?>
--- views/header.php
+++ views/header.php
@@ -4,9 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles/style.css">
+  <!-- google fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Sans+JP:wght@100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.0/css/all.min.css" integrity="sha512-ApSLB1Pd3/bZN8fWB/RG9YhN/7bd9Hkf3AGaE2mPfebjrxagjuBtx2GcgdqIlJkUzwylBo61r9Xa9NmgBI0swA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title><?php echo $title ?></title>
 </head>
 <body>
